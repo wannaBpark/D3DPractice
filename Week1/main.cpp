e71d9f74880f4c9558ab3d80617ae842ffc6deed
heonwoo <leehun456@ajou.ac.kr>
2025-01-16 23:37:06 +0900

    Step 1 : resizable window - resizable window through imgui (Swapchain, viewport info resize) - resized sphere for new resolution bug - reflect even it didn't reach left,right border

--- Week1/main.cpp
+++ Week1/main.cpp
@@ -1,4 +1,7 @@
+///////////////////////TODO : SWapchain 정보 변경
 #include <windows.h>
+
+#define SAFE_RELEASE( x ) {if(x){(x)->Release();(x)=NULL;}}
 
 // D3D 사용에 필요한 라이브러리들을 링크합니다.
 #pragma comment(lib, "user32")
@@ -15,6 +18,7 @@
 #include "ImGui/imgui_impl_dx11.h"
 #include "ImGui/imgui_impl_win32.h"
 
+#include <iostream>
 #include <algorithm> // clamp
 
 // 1. Define the triangle vertices 
@@ -27,6 +31,11 @@ struct FVertexSimple {
 struct FVector3 {
 	float x, y, z;
 	FVector3(float _x = 0, float _y = 0, float _z = 0) : x(_x), y(_y), z(_z) {}
+};
+
+struct FVector2 {
+	float x, y;
+	FVector2(float _x = 0, float _y = 0) : x(_x), y(_y) {}
 };
 
 #include "Sphere.h"
@@ -166,6 +175,8 @@ public:
 		ViewportInfo = { 0.0f, 0.0f, (float)swapchaindesc.BufferDesc.Width, (float)swapchaindesc.BufferDesc.Height, 0.0f, 1.0f };
 	}
 
+	
+
 	// Direct3D 장치 및 스왑 체인을 해제하는 함수
 	void ReleaseDeviceAndSwapChain()
 	{
@@ -173,26 +184,33 @@ public:
 			DeviceContext->Flush(); // 남아있는 GPU 명령 실행
 		}
 
+		SAFE_RELEASE(SwapChain);
+		SAFE_RELEASE(Device);
+		SAFE_RELEASE(DeviceContext);
+	}
+
+	// 스왑체인 크기 변경 (창이 더 커질 때 pixel이 늘어나보이는 현상 방지)
+	void ResizeSwapChain(int nxtWidth, int nxtHeight)
+	{
 		if (SwapChain) {
-			SwapChain->Release();
-			SwapChain = nullptr;
-		}
+			// [1] Swapchain과 관련한 Resource 해제 (pre-resize) : 기존 백버퍼인 FrameBuffer, RTV .. (DepthStencilView도 해야 함)
+			SAFE_RELEASE(FrameBuffer);			// 
+			SAFE_RELEASE(FrameBufferRTV);
 
-		if (Device) {
-			Device->Release();
-			Device = nullptr;
-		}
-
-		if (DeviceContext) {
-			DeviceContext->Release();
-			DeviceContext = nullptr;
+			// [2] SwapChain 크기 조정 (첫번째 인자 0이면 기존 스왑체인 버퍼 수 유지)
+			SwapChain->ResizeBuffers(0, nxtWidth, nxtHeight, DXGI_FORMAT_UNKNOWN, 0);
+			
+			// [3] 새로운 렌더 타겟 뷰 생성 / 크기에 맞게 viewport 크기 조정 (post-resize)
+			CreateFrameBuffer();
+			ViewportInfo.Width = nxtWidth;
+			ViewportInfo.Height = nxtHeight;
 		}
 	}
 
-	// 프레임 버퍼를 생성하는 함수 (텍
+	// [2] [프레임 버퍼를 생성하는 함수] : Swapchain 생성후 텍스처를 얻고, 렌더링용으로 파이프라인에 묶는다
 	void CreateFrameBuffer()
 	{
-		// 스왑 체인으로부터 백 버퍼 텍스처 가져오기 (지정된 인덱스 0)
+		// 스왑 체인으로부터 백 버퍼 텍스처 가져오기 (지정된 인덱스 0)  | 텍스처를 사용하려면 텍스처 인터페이스를 얻어와야 함
 		// 이 때 FrameBuffer는 렌더링된 이미지를 저장하는 데에 사용
 		SwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)&FrameBuffer);
 
@@ -207,14 +225,8 @@ public:
 	// 프레임 버퍼를 해제하는 함수
 	void ReleaseFrameBuffer()
 	{
-		if (FrameBuffer) {
-			FrameBuffer->Release();
-			FrameBuffer = nullptr;
-		}
-		if (FrameBufferRTV) {
-			FrameBufferRTV->Release();
-			FrameBufferRTV = nullptr;
-		}
+		SAFE_RELEASE(FrameBuffer);
+		SAFE_RELEASE(FrameBufferRTV);
 	}
 
 	// 래스터라이저 상태를 생성하는 함수
@@ -230,10 +242,7 @@ public:
 	// 래스터라이저 상태를 해제하는 함수
 	void ReleaseRasterizerState()
 	{
-		if (RasterizerState) {
-			RasterizerState->Release();
-			RasterizerState = nullptr;
-		}
+		SAFE_RELEASE(RasterizerState);
 	}
 
 	// 렌더러에 사용된 모든 리소스를 해제하는 함수
@@ -242,7 +251,7 @@ public:
 		RasterizerState->Release();
 
 		// 렌더 타겟을 초기화
-		DeviceContext->OMSetRenderTargets(0, nullptr, nullptr); // Deffered Context에선 OMS 안써도 문제없이 여러개 RTV에 그림
+		DeviceContext->OMSetRenderTargets(0, nullptr, nullptr); // 참고) Deffered Context에선 OMS 안써도 문제없이 여러개 RTV에 그림
 
 		ReleaseFrameBuffer();
 		ReleaseDeviceAndSwapChain();
@@ -280,20 +289,9 @@ public:
 	void ReleaseShader()
 	{
 		// Release는 코드 역순으로  호출
-		if (SimpleInputLayout) {
-			SimpleInputLayout->Release();
-			SimpleInputLayout = nullptr;
-		}
-
-		if (SimplePixelShader) {
-			SimplePixelShader->Release();
-			SimplePixelShader = nullptr;
-		}
-
-		if (SimpleVertexShader) {
-			SimpleVertexShader->Release();
-			SimpleVertexShader = nullptr;
-		}
+		SAFE_RELEASE(SimpleInputLayout);
+		SAFE_RELEASE(SimplePixelShader);
+		SAFE_RELEASE(SimpleVertexShader);
 	}
 
 	void Prepare()
@@ -366,10 +364,7 @@ public:
 
 	void ReleaseConstantBuffer()
 	{
-		if (ConstantBuffer) {
-			ConstantBuffer->Release();
-			ConstantBuffer = nullptr;
-		}
+		SAFE_RELEASE(ConstantBuffer);
 	}
 
 	// GPU와 묶인 ConstantBuffer를 임시로 MSR로 가져와 수정하고, 다시 떼어내어 GPU에 도로 묶음
@@ -420,9 +415,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	// 윈도우 클래스 등록
 	RegisterClassW(&wndclass);
 
+	FVector2 wndSize = { 1920, 900};
+	float aspectRatio = wndSize.x / wndSize.y;
 	// 1024 x 1024 크기에 윈도우 생성
 	HWND hWnd = CreateWindowExW(0, WindowClass, Title, WS_POPUP | WS_VISIBLE | WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, CW_USEDEFAULT, 1024, 1024,
+		CW_USEDEFAULT, CW_USEDEFAULT, wndSize.x, wndSize.y,
 		nullptr, nullptr, hInstance, nullptr);
 
 	// Renderer Class를 생성
@@ -440,10 +437,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	ImGui_ImplDX11_Init(renderer.Device, renderer.DeviceContext);
 
 	// 렌더러,셰이더 생성 후 VertexBuffer 생성	
-	// [질문1] (numvertices는 다른 구조체를 가질 때 코드가 변경되는데, 이를 매크로로 자동으로 추가할 방법 + 소유권 누구한테 줘야 적당할지
+	// [복습] (numvertices는 다른 구조체를 가질 때 코드가 변경되는데, 이를 매크로로 자동으로 추가할 방법 + 소유권 누구한테 줘야 적당할지
 	UINT numVerticesTriangle = sizeof(triangle_vertices) / sizeof(FVertexSimple);
 	UINT numVerticesCube = sizeof(cube_vertices) / sizeof(FVertexSimple);
 	UINT numVerticesSphere = sizeof(sphere_vertices) / sizeof(FVertexSimple);
+
+	std::unique_ptr<FVertexSimple[]> nxtsphere_vertices{ new FVertexSimple[numVerticesSphere] }; // 해상도에 따라 변경될 구 정점 저장할 배열
 
 	// Vertex Buffer로 넘기기 전에 Scale Down함
 	float scaleMod = 0.1f;
@@ -452,6 +451,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		sphere_vertices[i].y *= scaleMod;
 		sphere_vertices[i].z *= scaleMod;
 	}
+	std::copy(sphere_vertices, sphere_vertices+numVerticesSphere, nxtsphere_vertices.get());
 
 	ID3D11Buffer* vertexBufferTriangle = renderer.CreateVertexBuffer(triangle_vertices, sizeof(triangle_vertices));
 	ID3D11Buffer* vertexBufferCube = renderer.CreateVertexBuffer(cube_vertices, sizeof(cube_vertices));
@@ -530,6 +530,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 			offset.y += velocity.y;
 			offset.z += velocity.z;
 
+
 			float renderRadius = sphereRadius * scaleMod;
 			// 벽과 충돌 여부를 체크하고 충돌시 속도에 음수를 곱해 방향을 바꿈
 			if (offset.x < leftBorder + renderRadius) {
@@ -565,12 +566,39 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		ImGui::Text("Hello Jungle world!");
 		ImGui::Checkbox("Bound Ball to Screen", &bBoundBallToScreen);
 		ImGui::Checkbox("Pinball Movement", &bPinballMovement);
+		ImGui::InputFloat2("Change Window Size", &wndSize.x, "%.0f");
 
+		if (ImGui::Button("Apply size Change")) {
+			// 윈도우 크기 변경
+			RECT rect = { 0, 0, static_cast<LONG>(wndSize.x), static_cast<LONG>(wndSize.y) };
+			AdjustWindowRect(&rect, WS_POPUP | WS_VISIBLE | WS_OVERLAPPEDWINDOW, FALSE);
+			SetWindowPos(hWnd, NULL, CW_USEDEFAULT, CW_USEDEFAULT, (int)wndSize.x, (int)wndSize.y, SWP_NOMOVE | SWP_NOZORDER);
 
-		ImGui::End();
-		ImGui::Render();
-		ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData()); // 렌더링 요청
+			renderer.ResizeSwapChain(static_cast<int>(wndSize.x), static_cast<int>(wndSize.y));
 
+			// ImGui 충돌범위도 변경 위해 디스플레이 크기와 프레임버퍼 크기 설정 / ImGui 소멸
+			ImGui_ImplDX11_Shutdown();
+			ImGui_ImplWin32_Shutdown();
+			ImGui::DestroyContext();
+
+			// 새로운 컨텍스트 생성, 새로운 hWnd로 초기화
+			ImGui::CreateContext();
+			ImGui_ImplWin32_Init((void*)hWnd);
+			ImGui_ImplDX11_Init(renderer.Device, renderer.DeviceContext);
+
+			SAFE_RELEASE(vertexBufferSphere);
+			
+			float aspectRatio = wndSize.x / wndSize.y;
+			for (UINT i = 0; i < numVerticesSphere; ++i) {
+				nxtsphere_vertices[i].x = (aspectRatio > 1.0f) ? sphere_vertices[i].x / aspectRatio : sphere_vertices[i].x;
+				nxtsphere_vertices[i].y = (aspectRatio < 1.0f) ? sphere_vertices[i].y * aspectRatio : sphere_vertices[i].y;
+			}
+			vertexBufferSphere = renderer.CreateVertexBuffer(nxtsphere_vertices.get(), sizeof(sphere_vertices));
+		} else {
+			ImGui::End();
+			ImGui::Render();
+			ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData()); // 렌더링 요청
+		}
 		// [3] 현재 화면에 보여지는 버퍼와 그리기 작업을 위한 버퍼를 서로 교환
 		renderer.SwapBuffer();
 
@@ -584,7 +612,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		} while (elapsedTime < targetFrameTime);
 		
 	}
-	
+
 	// ImGui 소멸
 	ImGui_ImplDX11_Shutdown();
 	ImGui_ImplWin32_Shutdown();
